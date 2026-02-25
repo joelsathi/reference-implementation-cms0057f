6fc5855eaa0d36825897bbd715d783e5317d5ab9
isuruh15 <isurus@wso2.com>
2026-02-25 12:49:42 +0530

    fix build failures

---
+++ apps/payer-admin-app/public/config.js
@@ -0,0 +1,8 @@
+window.config = {
+    BFF_URL: "http://localhost:6091/v1",
+    PDEX_API_URL: "http://localhost:8091/pdex"
+};
+// window.configs = {
+//     BFF_URL: '/choreo-apis/cms-paas/wso2payerportalbff/v1',
+//     PDEX_API_URL: "/choreo-apis/cms-paas/fhir-server/v1"
+// };
